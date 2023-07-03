c8d395525fe5bcb1b6bc596ee8c06fcb0e22438a
shru <shruthishekar@gmail.com>
2023-07-03 10:44:02 -0600

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,4 +4,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network');
 
 //export connection
-module.exports.mongoose.connection;
+module.exports = mongoose.connection;
